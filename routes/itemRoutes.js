adbca5511508fd407fc601ecc54349d7e83b9a7c
IdrisLaabidi <idrislaabidi@gmail.com>
2024-03-30 22:42:23 +0100

    collections and items api done

---
+++ routes/itemRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+const {addItem,getAllItems,updateItem,deleteItem,getItemsinCollection,getItem} = require('../controllers/itemController')
+const { protect } = require('../middleware/authMiddleware');
+
+router.post('/',protect,addItem)
+
+router.get('/',protect,getAllItems)
+
+router.get('/:id',protect,getAllItems)
+
+router.get('/collection/:collectionId',protect,getItemsinCollection)
+
+router.put('/:id',protect,updateItem)
+
+router.delete('/:id',protect,deleteItem)
+
+module.exports = router
