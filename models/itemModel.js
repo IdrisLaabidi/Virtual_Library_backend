f5019aa623e3dc68ebb9da9a81763f69576def6e
IdrisLaabidi <idrislaabidi@gmail.com>
2024-03-31 14:58:32 +0100

    items model

---
+++ models/itemModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const ItemSchema = new mongoose.Schema({
+    id : mongoose.Schema.Types.ObjectId,
+    collection : { type: mongoose.Schema.Types.ObjectId, ref: 'Collection' , required : true}, //the collection containing this item
+    type : { type:String , enum : ['livre','film','musique','jeux-video'], required : true},
+    titre : { type : String , required : true},
+    description : { type : String },
+    editor : { type : String },
+    imageLink : { type : String },
+    isbn : { type : Number },
+    price : { type : Number },
+    pageNumber : { type : Number },
+
+})
+module.exports = mongoose.model('Item',ItemSchema)
