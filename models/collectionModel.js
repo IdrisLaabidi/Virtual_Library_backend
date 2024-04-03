9d10b351dbbac8db44fc04a873f7380dd2fc98c3
tasnimebbk <tasnime@gmail.com>
2024-04-03 16:01:56 +0100

    fixed collection

--- models/collectionModel.js
+++ models/collectionModel.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const CollectionSchema = new mongoose.Schema({
     id : mongoose.Schema.Types.ObjectId,
     title : {type : String , required : true},
-    language : { type : String , enum : ['Arabic', 'French' , 'English','German','Italic']},
+    language : { type : String , enum : ['AR', 'FR' , 'EN','DE','IT']},
     items : [{type: mongoose.Schema.Types.ObjectId, ref: 'Item'}]
 })
 
