5d757197e66215084caf36c48c02b5e28b1f5c33
Youssef Fathallah <fathallahyoussef09@gmail.com>
2024-04-06 17:22:27 +0100

    update deleteItem and updateItem controllers, 85% functionnal

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -37,11 +37,24 @@ const getAllItems = expressAsyncHandler(async (req,res) =>{
 
 const updateItem = expressAsyncHandler(async (req,res) =>{
     try {
+        //get the item to be updated
+        const item = await Item.findById(req.params.id)
+        //get the old collection
+        const oldCollection = await Collection.findById(item.collection)
+        //remove the item's id from the old collection's array
+        oldCollection.items.pull(item.id)
+        await oldCollection.save();
+        //update the item
         const updatedItem = await Item.findOneAndUpdate(
             { _id: req.params.id },
             req.body,
             { new: true }
         );
+        //get the new collection
+        const newCollection = await Collection.findById(updatedItem.collection)
+        //add the item's id to he array of the newly selected collection
+        newCollection.items.push(updatedItem.id)
+        await newCollection.save();
         res.status(200).json({ updatedItem });
     } catch (error) {
         res.status(400).json({ message: error.message });
@@ -50,6 +63,14 @@ const updateItem = expressAsyncHandler(async (req,res) =>{
 
 const deleteItem = expressAsyncHandler(async (req,res) =>{
     try {
+        // Get the item to be deleted
+        const item = await Item.findById(req.params.id);
+        // Get the collection
+        const collection = await Collection.findById(item.collection);
+        // Remove the item's id from the collection's array
+        collection.items.pull(item.id);
+        await collection.save();
+        // Delete the item
         const deletedItem = await Item.findOneAndDelete({ _id: req.params.id });
         res.status(200).json({ deletedItem });
     } catch (error) {
