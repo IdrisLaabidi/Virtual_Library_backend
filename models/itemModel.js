ea44c6ee937dc25414e57971d70109cb96d7a264
RayenMiri <rayenmiri000@gmail.com>
2024-04-13 10:55:53 +0100

    added collection image url in item controller

--- models/itemModel.js
+++ models/itemModel.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose')
 
 const ItemSchema = new mongoose.Schema({
     id : mongoose.Schema.Types.ObjectId,
-    collection : { type: mongoose.Schema.Types.ObjectId, ref: 'Collection' , required : true}, //the collection containing this item
+    group : { type: mongoose.Schema.Types.ObjectId, ref: 'Collection' , required : true}, //the collection containing this item
     type : { type:String , enum : ['livre','film','musique','jeux-video'], required : true},
     titre : { type : String , required : true},
+    auteur : { type: String },
     description : { type : String },
     editor : { type : String },
     imageLink : { type : String },
