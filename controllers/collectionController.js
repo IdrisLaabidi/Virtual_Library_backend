a6295b23295897ae2473cbe1a3069fa940cdb14a
IdrisLaabidi <idrislaabidi@gmail.com>
2024-04-27 23:25:18 +0100

    share a collection controller

--- controllers/collectionController.js
+++ controllers/collectionController.js
@@ -1,6 +1,9 @@
 const expressAsyncHandler = require('express-async-handler');
 const Collection = require('../models/collectionModel')
 const Item = require('../models/itemModel')
+const User = require('../models/userModel')
+const mongoose = require('mongoose');
+const { collection } = require('firebase/firestore');
 
 const addCollection = expressAsyncHandler(async (req, res) => {
     try {
@@ -14,14 +17,22 @@ const addCollection = expressAsyncHandler(async (req, res) => {
 const getAllCollections = expressAsyncHandler(async (req, res) => {
     try {
         const userId = req.params.userid; 
-        console.log(userId)
-        const toutesLesCollections = await Collection.find({ 'user': userId });
-        res.status(200).json(toutesLesCollections);
+        console.log(userId);
+        let toutesLesCollections = await Collection.find({ 'user': userId });
+        const user = await User.findById(userId);
+        let collections = [...toutesLesCollections]; // Initialize with toutesLesCollections
+        let shared
+        if (user.sharedCollections?.length !== 0) {
+            shared = await Collection.find({ _id: { $in: user.sharedCollections } });
+            collections = collections.concat(shared); // Combine toutesLesCollections and shared
+        }
+        res.status(200).json(collections); // Return the combined collections
     } catch (error) {
         console.error('Error fetching collections:', error);
         res.status(500).json({ message: 'Internal server error' });
     }
 });
+
 
 const getCollection = expressAsyncHandler(async (req, res) => {
     try {
@@ -66,7 +77,20 @@ const deleteCollection = expressAsyncHandler(async (req, res) => {
     }
 });
 
-module.exports = {addCollection,getAllCollections,updateCollection,deleteCollection,getCollection}
+const handleShare = expressAsyncHandler( async (req,res) => {
+    try{
+        const modifiedUsers = await User.updateMany(
+            { _id: { $in: req.body } },
+            { $push: { sharedCollections: req.params.id } },
+            { new: true }
+        );
+        res.status(200).json({message : 'ok'})
+    }catch(error){
+        console.log(error)
+    }
+})
+
+module.exports = {addCollection,getAllCollections,updateCollection,deleteCollection,getCollection,handleShare}
 
 
 
